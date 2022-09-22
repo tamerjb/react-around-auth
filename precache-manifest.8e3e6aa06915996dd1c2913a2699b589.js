130ae0dc6a6cbc7b9e15232a031ce9b8c11244c5
Tamer Jb <tamerjb.98@gmail.com>
2022-09-22 13:09:31 +0300

    Updates

---
+++ precache-manifest.8e3e6aa06915996dd1c2913a2699b589.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "692ff0d65e72f8ab274e",
+    "url": "/react-around-auth/static/css/main.072e8183.chunk.css"
+  },
+  {
+    "revision": "692ff0d65e72f8ab274e",
+    "url": "/react-around-auth/static/js/main.692ff0d6.chunk.js"
+  },
+  {
+    "revision": "6d901b3e33806cc9ca52",
+    "url": "/react-around-auth/static/js/1.6d901b3e.chunk.js"
+  },
+  {
+    "revision": "fab4700e70fef87a01ca",
+    "url": "/react-around-auth/static/js/2.fab4700e.chunk.js"
+  },
+  {
+    "revision": "d4d71a042ebe72e432d1",
+    "url": "/react-around-auth/static/js/runtime~main.d4d71a04.js"
+  },
+  {
+    "revision": "8f7611ae39c682f37516feec261f77dd",
+    "url": "/react-around-auth/static/media/around.8f7611ae.svg"
+  },
+  {
+    "revision": "e38b395c190826b3bf6d78baff1415a0",
+    "url": "/react-around-auth/static/media/successful_icon.e38b395c.svg"
+  },
+  {
+    "revision": "4072126c596bd9485c6e9601f6d4d35d",
+    "url": "/react-around-auth/static/media/unsuccessful_icon.4072126c.svg"
+  },
+  {
+    "revision": "3ac83020fe53b617b79b5e2ad66764af",
+    "url": "/react-around-auth/static/media/Inter-Regular.3ac83020.woff"
+  },
+  {
+    "revision": "dc131113894217b5031000575d9de002",
+    "url": "/react-around-auth/static/media/Inter-Regular.dc131113.woff2"
+  },
+  {
+    "revision": "7ee866f113eb2ed4216d588f5b956e6e",
+    "url": "/react-around-auth/static/media/trash.7ee866f1.svg"
+  },
+  {
+    "revision": "75db5319e7e87c587019a5df08d7272c",
+    "url": "/react-around-auth/static/media/Inter-Medium.75db5319.woff2"
+  },
+  {
+    "revision": "c0638bea87a05fdfa2bb3bba2efe54e4",
+    "url": "/react-around-auth/static/media/Inter-Medium.c0638bea.woff"
+  },
+  {
+    "revision": "9293aa8fd18af383d6eb4eeeecab483f",
+    "url": "/react-around-auth/static/media/close_icon.9293aa8f.svg"
+  },
+  {
+    "revision": "dd31dd063f1c376deb28b582c4a64833",
+    "url": "/react-around-auth/static/media/like.dd31dd06.svg"
+  },
+  {
+    "revision": "6bfdb35467f51d3ba1f70f8d4a9c2b59",
+    "url": "/react-around-auth/static/media/edit_icon.6bfdb354.svg"
+  },
+  {
+    "revision": "661569afe57a38e1529a775a465da20b",
+    "url": "/react-around-auth/static/media/Inter-Black.661569af.woff2"
+  },
+  {
+    "revision": "b2fce4ac45f3d116cd48ec8d068ab723",
+    "url": "/react-around-auth/static/media/plus_icon.b2fce4ac.svg"
+  },
+  {
+    "revision": "ef38b085db5a81756d4dc5a64c1fccb8",
+    "url": "/react-around-auth/static/media/like_filled.ef38b085.svg"
+  },
+  {
+    "revision": "d0b121f3a9d3d88afdfd6902d31ee9a0",
+    "url": "/react-around-auth/static/media/Inter-Black.d0b121f3.woff"
+  },
+  {
+    "revision": "08e793869b61a98f28f90d508afd4d71",
+    "url": "/react-around-auth/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/react-around-auth/precache-manifest.8e3e6aa06915996dd1c2913a2699b589.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/react-around-auth/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
